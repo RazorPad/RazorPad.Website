2088f51579fc38547593ab409a69572510ddaf66
Jess Chadwick <jesschadwick@gmail.com>
2012-02-17 00:12:10 -0500

    Moving src to root

--- src/RazorPad.Core/Compilation/CSharpRazorCodeGeneratorSpanVisitor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Web.Razor.Parser.SyntaxTree;
-
-namespace RazorPad.Compilation
-{
-    public abstract class CSharpRazorCodeGeneratorSpanVisitor
-    {
-        public abstract bool TryVisit(Span span);
-    }
-}
--- src/RazorPad.Core/Compilation/ViewComponentCSharpRazorCodeParser.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.Mvc.Razor;
-
-namespace RazorPad.Compilation
-{
-    public class RazorPadCSharpRazorCodeParser : MvcCSharpRazorCodeParser
-    {
-    }
-}
