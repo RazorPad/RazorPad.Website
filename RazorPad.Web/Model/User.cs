19a83e3f9f144a55756194a75bec7f86c4e713b7
Jess Chadwick <jesschadwick@gmail.com>
2012-03-09 11:17:03 -0500

    Adding Credentials extensions

--- RazorPad.Web/Model/User.cs
+++ RazorPad.Web/Model/User.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using RazorPad.Web.Services;
 
 namespace RazorPad.Web
 {
@@ -20,6 +22,44 @@ namespace RazorPad.Web
         public User()
         {
             Credentials = new Collection<Credential>();
+        }
+
+
+        public TCredential GetCredential<TCredential>(Func<TCredential, bool> predicate = null) where TCredential : Credential
+        {
+            return GetCredentials(predicate).SingleOrDefault();
+        }
+
+        public IEnumerable<TCredential> GetCredentials<TCredential>(Func<TCredential, bool> predicate = null) where TCredential : Credential
+        {
+            var credentials = Credentials.OfType<TCredential>();
+
+            if (predicate != null)
+                return credentials.Where(predicate);
+
+            return credentials;
+        }
+    }
+
+
+    public static class UserRespositoryExtensions
+    {
+        public static User FindUserByCredential<TCredential>(this IRepository repository, Func<TCredential, bool> predicate = null)
+            where TCredential : Credential
+        {
+            var user = repository.SingleOrDefault<User>(u => u.GetCredentials(predicate).Any());
+
+            return user;
+        }
+
+        public static User FindUserByEmail(this IRepository repository, string emailAddress)
+        {
+            return repository.SingleOrDefault<User>(user => user.EmailAddress == emailAddress);
+        }
+
+        public static User FindUserByUsername(this IRepository repository, string username)
+        {
+            return repository.SingleOrDefault<User>(user => user.Username == username);
         }
     }
 }
