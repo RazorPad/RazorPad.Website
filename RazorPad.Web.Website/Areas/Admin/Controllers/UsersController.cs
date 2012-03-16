d7ba492434d2f115c172331da38529e5d63596f0
Jess Chadwick <jesschadwick@gmail.com>
2012-03-16 17:29:28 -0400

    Adding Admin area

---
+++ RazorPad.Web.Website/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Raven.Client;
+using RazorPad.Web.Services;
+
+namespace RazorPad.Web.Website.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IRepository _repository;
+        private readonly IDocumentSession _session;
+
+        public UsersController(IRepository repository, IDocumentSession session)
+        {
+            _repository = repository;
+            _session = session;
+        }
+
+        public ActionResult Index()
+        {
+            var users = _repository.Query<User>();
+            return View("Users", users);
+        }
+
+        public ActionResult Delete(long id)
+        {
+            var user = _repository.SingleOrDefault<User>(u => u.Id == id);
+            _session.Delete(user);
+
+            if(Request.IsAjaxRequest())
+                return new HttpStatusCodeResult(204);
+
+            TempData.Add("Message", "Deleted user "+id);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteAll()
+        {
+            IQueryable<User> users = _repository.Query<User>();
+
+            foreach (var user in users)
+            {
+                _session.Delete(user);
+            }
+
+            if (Request.IsAjaxRequest())
+                return new HttpStatusCodeResult(204);
+
+            TempData.Add("Message", "Deleted "+users.Count()+"users");
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- RazorPad.Web/Model/Fiddle.cs
+++ RazorPad.Web/Model/Fiddle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Web.Script.Serialization;
 using RazorPad.Web.Dynamic;
 
@@ -7,10 +8,17 @@ namespace RazorPad.Web
     public class Fiddle
     {
         public string Id { get; set; }
+        
         public string Title { get; set; }
+        
         public string Notes { get; set; }
+
+        [StringLength(50000, ErrorMessage = "You can't save this view -- it's way too big!  Try something under 50k characters.")]
         public string View { get; set; }
+
+        [StringLength(25000, ErrorMessage = "You can't save this model -- it's way too big!  Try something under 25k characters.")]
         public string InputModel { get; set; }
+
         public dynamic DynamicInputModel
         {
             get
@@ -18,15 +26,19 @@ namespace RazorPad.Web
                 if (string.IsNullOrEmpty(InputModel))
                     return null;
 
-                JavaScriptSerializer jss = new JavaScriptSerializer();
-                jss.RegisterConverters(new JavaScriptConverter[] { new DynamicJsonConverter() });
+                var serializer = new JavaScriptSerializer();
+                serializer.RegisterConverters(new JavaScriptConverter[] { new DynamicJsonConverter() });
 
-                return jss.Deserialize(InputModel, typeof(object));
+                return serializer.Deserialize(InputModel, typeof(object));
             }
         }
+
         public string Language { get; set; }
+
         public string Version { get; set; }
+
         public DateTime DateCreated { get; set; }
+
         public string CreatedBy { get; set; }
     }
 }
