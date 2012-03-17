a1401eda041fe104a518746f02a19682699b0bc9
Jess Chadwick <jesschadwick@gmail.com>
2012-03-16 21:41:53 -0400

    Fixing delete user

--- RazorPad.Web.Website/Areas/Admin/Controllers/UsersController.cs
+++ RazorPad.Web.Website/Areas/Admin/Controllers/UsersController.cs
@@ -24,8 +24,7 @@ namespace RazorPad.Web.Website.Areas.Admin.Controllers
 
         public ActionResult Delete(long id)
         {
-            var user = _repository.SingleOrDefault<User>(u => u.Id == id);
-            _session.Delete(user);
+            _session.Advanced.DatabaseCommands.Delete("users-"+id, null);
 
             if(Request.IsAjaxRequest())
                 return new HttpStatusCodeResult(204);
@@ -34,23 +33,5 @@ namespace RazorPad.Web.Website.Areas.Admin.Controllers
 
             return RedirectToAction("Index");
         }
-
-        public ActionResult DeleteAll()
-        {
-            IQueryable<User> users = _repository.Query<User>();
-
-            foreach (var user in users)
-            {
-                _session.Delete(user);
-            }
-
-            if (Request.IsAjaxRequest())
-                return new HttpStatusCodeResult(204);
-
-            TempData.Add("Message", "Deleted "+users.Count()+"users");
-
-            return RedirectToAction("Index");
-        }
-
     }
 }
