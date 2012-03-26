381443157393609dffca141e0ec438eccdd126b5
Jess Chadwick <jesschadwick@gmail.com>
2012-03-26 16:58:39 -0400

    Cleaning up JS

--- RazorPad.Web.Website/Scripts/RazorPad.js
+++ RazorPad.Web.Website/Scripts/RazorPad.js
@@ -1,4 +1,89 @@
-﻿var RazorPad = typeof(RazorPad) == 'undefined' ? {} : RazorPad;
+﻿$(function () {
+
+    RazorPad.razorEditor = null;
+
+    function resizeRazorEditor() {
+        $(RazorPad.razorEditor.getScrollerElement()).css({
+            height: $("#razorPane").height() - $("#viewHeader").height(),
+            width: $("#razorPane").width()
+        });
+        RazorPad.razorEditor.refresh();
+        resizeModelProps();
+    }
+
+    function resizeModelProps() {
+        var $modelPane = $('#modelPane');
+        $modelPane.find('.modelProps')
+                         .height(
+                            $modelPane.height()
+                            - $modelPane.find('.paneheader').outerHeight(true)
+                            - $modelPane.find('.propinput').outerHeight(true)
+                            - 10//modelProps top and bottom padding
+                            );
+    }
+
+    var outerLayout = $('body').layout({
+        north__paneSelector: "#toolbar",
+        north__resizable: false,
+        west__paneSelector: "#sidebar",
+        west__size: Math.floor((screen.width / 1.67) - 10),
+        west__resizable: false,
+        west__closable: true,
+        west__maxSize: 230,
+        center__paneSelector: "#panes",
+        resizeWhileDragging: true,
+        closable: false
+    });
+
+    var innerLayout = $('#panes').layout({
+        closable: false,
+        center__paneSelector: "#razorPane",
+        east__paneSelector: "#modelPane",
+        east__maxSize: 600,
+        east__minSize: 400,
+        east__size: 400,
+        south__paneSelector: "#resultsPane",
+        south__maxSize: "70%",
+        south__size: Math.floor((screen.height / 2) - 100),
+        south__minSize: 200,
+        resizeWhileDragging: true,
+        onresize: resizeRazorEditor
+    });
+
+
+    RazorPad.razorEditor = CodeMirror.fromTextArea(document.getElementById("razorEditor"), {
+        mode: "text/html",
+        lineNumbers: true,
+        tabMode: "indent",
+        matchBrackets: true,
+        tabIndex: -1,
+        lineWrapping: true,
+        extraKeys: {
+            "Ctrl-S": function () { RazorPad.saveTemplate(); },
+            "Ctrl-E": function () { RazorPad.executeTemplate(); }
+        }
+    });
+
+    $("#mainContainer").css('visibility', 'visible');
+
+    $(window).resize(resizeRazorEditor).resize();
+
+    if ($("#fiddleId").val()) {
+        //Execute the template (adding timeout to load the layout before ajax calls)
+        setTimeout(function () {
+            RazorPad.executeTemplate();
+        }, 200);
+    }
+
+    $('#savedFiddles').height($("#sidebar").height() / 2);
+
+    $('#accordion').accordion({
+        autoHeight: false,
+        change: function () {
+            $('#savedFiddles').height($("#sidebar").height() / 2);
+        }
+    });
+});
 
 RazorPad.saveTemplate = function (clone) {
     RazorPad.showLoading();
@@ -37,42 +122,48 @@ RazorPad.executeTemplate = function() {
     RazorPad.showLoading();
     var data = {
         Template: RazorPad.razorEditor.getValue(),
-        Model:  JSON.stringify(RazorPad.getModel())
+        Model: JSON.stringify(RazorPad.getModel())
     };
 
     $.ajax({
         url: RazorPad.siteRoot + 'RazorPad/Execute',
         data: JSON.stringify(data),
-        success: function (resp) {
+        success: function(resp) {
             RazorPad.onParseSuccess(resp);
             RazorPad.showRenderedTemplateOutput(resp.TemplateOutput);
             $('#template-output').text(resp.TemplateOutput);
             $('div.tabContainer').hpTabs("select", resp.Success ? 0 : 4); //Select Output tab
         },
-        error: function (resp) {
+        error: function(resp) {
             RazorPad.onParseError(resp);
             var message = ' [[**** EXECUTION ERROR ****]] \r\n' + JSON.stringify(resp);
             $('#rendered-output-container').empty().text(message);
             $('#template-output').text(message);
             $('div.tabContainer').hpTabs("select", 4); //Select Message tab
         },
-        complete: function () {
+        complete: function() {
             RazorPad.hideLoading();
         }
     });
-}
+};
 
 RazorPad.handleSaveKey = function(evt) {
     RazorPad.saveTemplate();
     evt.stopPropagation();
     return false;
-}
+};
 
 RazorPad.handleExecuteKey = function(evt) {
     RazorPad.executeTemplate();
     evt.stopPropagation();
     return false;
-}
+};
+
+RazorPad.handleCloneKey = function(evt) {
+    RazorPad.saveTemplate(true);
+    evt.stopPropagation();
+    return false;
+};
 
 RazorPad.showLoading = function() {
     var $loading = $('#loading');
@@ -80,70 +171,73 @@ RazorPad.showLoading = function() {
         left: ($(document).width() - $loading.width()) / 2,
         top: ($(document).height() - $loading.height()) / 2
     }).fadeIn('fast');
-}
+};
 
 RazorPad.hideLoading = function() {
     $('#loading').fadeOut('slow');
-}
+};
 
-RazorPad.getModel = function () {
-    var model = {}, $this, name, val, type;
-    $('#modelGrid > tbody > tr').each(function () {
+RazorPad.getModel = function() {
+    var model = { }, $this, name, val, type;
+    $('#modelGrid > tbody > tr').each(function() {
         $this = $(this);
         name = $this.find('span.name').text();
         val = $this.find('span.value').text();
-        type = $this.data('type') || "string";//If type is not specified consider it as string
+        type = $this.data('type') || "string"; //If type is not specified consider it as string
         if (name && val) {
             model[$.trim(name)] = RazorPad.parsePropValue($.trim(val), $.trim(type));
         }
     });
     return model;
-}
+};
 
-RazorPad.parsePropValue = function (val, type) {
+RazorPad.parsePropValue = function(val, type) {
     if (type && val) {
         switch (type.toLowerCase()) {
-            case "int":
-                return parseInt(val);
-            case "float":
-            case "double":
-                return parseFloat(val);
-            case "string":
-                return val;
+        case "int":
+            return parseInt(val);
+        case "float":
+        case "double":
+            return parseFloat(val);
+        case "string":
+            return val;
         }
     }
     return val;
-}
+};
 
 RazorPad.onParseError = function(err) {
     RazorPad.updateStatus('fail');
-    RazorPad.showMessages([{ Kind: 'Error', Text: JSON.stringify(err)}]);
+    RazorPad.showMessages([{ Kind: 'Error', Text: JSON.stringify(err) }]);
     $('#generated-code').html(' [[**** PARSE ERROR ****]] ');
-} // END onParseError()
+};
 
 RazorPad.onParseSuccess = function(resp) {
-    if (resp.Success) { RazorPad.updateStatus('success'); }
-    else { RazorPad.updateStatus('fail'); }
+    if (resp.Success) {
+        RazorPad.updateStatus('success');
+    } else {
+        RazorPad.updateStatus('fail');
+    }
 
     RazorPad.showMessages(resp.Messages);
     $('#generated-code-container').empty().append($('<pre id="generated-code" class="brush: csharp"></pre>').text(resp.GeneratedCode));
 
     $('#parser-result-container').empty().append($('<pre id="parser-results" class="brush: html"></pre>').text(resp.ParsedDocument));
     SyntaxHighlighter.highlight({ toolbar: false });
-} // END onParseSuccess()
+};
 
 
 RazorPad.showMessages = function(messages) {
     var messagesList = $('#messages');
     messagesList.html('');
 
-    $.each(messages, function (idx, message) {
+    $.each(messages, function(idx, message) {
         $('<li/>')
-			.addClass(message.Kind)
-			.html($('<pre/>').html(message.Text))
-			.appendTo(messagesList);
+            .addClass(message.Kind)
+            .html($('<pre/>').html(message.Text))
+            .appendTo(messagesList);
     });
-} // END showMessages()
+};
 
 RazorPad.showRenderedTemplateOutput = function(templateOutput) {
     var iframe = $('iframe', '#rendered-output-container');
@@ -152,11 +246,11 @@ RazorPad.showRenderedTemplateOutput = function(templateOutput) {
         iframe = $('<iframe>').appendTo('#rendered-output-container');
 
     iframe.contents().find('body').html(templateOutput);
-}
+};
 
-RazorPad.updateStatus = function (status) {
+RazorPad.updateStatus = function(status) {
     $('#template-container').attr('class', status);
-}
+};
 
 
 
@@ -178,8 +272,7 @@ $(function () {
     });
 
     if ($("#fiddleId").val()) {
-        $('#clone')
-        .click(function (e) {
+        $('#clone').click(function (e) {
             e.preventDefault();
             RazorPad.saveTemplate(true);
         }).parent().show();
@@ -194,7 +287,8 @@ $(function () {
     $.fn.bindHotkeys = function () {
         this
         .bind('keydown', 'ctrl+s', RazorPad.handleSaveKey)
-        .bind('keydown', 'ctrl+e', RazorPad.handleExecuteKey);
+        .bind('keydown', 'ctrl+e', RazorPad.handleExecuteKey)
+        .bind('keydown', 'ctrl+c', RazorPad.handleCloneKey);
     };
 
     $(document).bindHotkeys();
